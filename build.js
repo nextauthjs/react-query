6352dad6aae61385b9b6c0c25030ca78023b6c66
ndom91 <yo@ndo.dev>
2021-06-04 17:20:58 +0200

    fix: missing build.js

---
+++ build.js
@@ -0,0 +1,29 @@
+const build = require("esbuild");
+
+build
+	.build({
+		entryPoints: ["index.js"],
+		bundle: true,
+		minify: true,
+		loader: {
+			".js": "ts"
+		},
+		format: "esm",
+		external: ["react", "react-dom"],
+		outfile: "dist/index.mjs"
+	})
+	.catch(() => process.exit(1));
+
+build
+	.build({
+		entryPoints: ["index.js"],
+		bundle: true,
+		minify: true,
+		loader: {
+			".js": "ts"
+		},
+		format: "cjs",
+		external: ["react", "react-dom"],
+		outfile: "dist/index.js"
+	})
+	.catch(() => process.exit(1));
